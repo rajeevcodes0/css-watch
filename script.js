c6c27c5f6c6a9d903c310f468bc8bff8d87a04f0
Rajeev Pandey <rajeevpandey.devac@gmail.com>
2022-03-14 04:33:07 +0530

    created bricks using css and some other ui imporvements

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
+function insertBricks(){
+  let bricksContainer = document.getElementsByClassName("bricks-container")[0];
+
+  let evenRow = true;
+  for(let i=0;i<40;i++){
+    if(evenRow){
+      let newRow = document.createElement("div");
+
+      bricksContainer.appendChild(newRow);
+
+      newRow.className="brick-row-even";
+
+      for(let j=0;j<30;j++){
+        let newBrick = document.createElement("div");
+        newBrick.className="brick";
+        let randomNumber = Math.random()*10;
+        if(randomNumber<=2){
+          newBrick.style.backgroundColor="#0e0e0e"
+        }
+        newRow.appendChild(newBrick);
+      }
+      evenRow=false
+    }else{
+      let newRow = document.createElement("div");
+
+      bricksContainer.appendChild(newRow);
+
+      newRow.className="brick-row-odd";
+
+      for(let j=0;j<30;j++){
+        let newBrick = document.createElement("div");
+        newBrick.className="brick";
+        newRow.appendChild(newBrick);
+      }
+
+      evenRow=true;
+    }
+   
+
+  }
+}
+
+
 function init() {
+  insertBricks();
   //grab the needles
   let secondNeedle = document.querySelector(".second-needle");
   let minuteNeedle = document.querySelector(".minute-needle");
