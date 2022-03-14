f34113406f8b57bb95954667113e7101c03afce9
Rajeev Pandey <rajeevpandey.devac@gmail.com>
2022-03-15 03:13:54 +0530

    added guitar svgs and some music, party mode on

--- script.js
+++ script.js
@@ -40,9 +40,36 @@ function insertBricks(){
   }
 }
 
+function addListenersToGuitars(){
+  let trackOne = document.getElementById("neon-guitar-one-track");
+  let guitarOne = document.getElementById("neon-guitar-one");
+  let musicIconOne = document.getElementById("neon-guitar-one-music-icon");
+  let trackTwo = document.getElementById("neon-guitar-two-track");
+  let guitarTwo = document.getElementById("neon-guitar-two");
+  let musicIconTwo = document.getElementById("neon-guitar-two-music-icon");
+
+  //hide both icons
+  musicIconOne.style.opacity="0";
+  musicIconTwo.style.opacity="0";
+
+  guitarOne.addEventListener("click",()=>{
+    musicIconOne.style.opacity="1";
+    musicIconTwo.style.opacity="0";
+    trackTwo.pause();
+    trackOne.play();
+  })
+
+  guitarTwo.addEventListener("click",()=>{
+    musicIconOne.style.opacity="0";
+    musicIconTwo.style.opacity="1";
+    trackOne.pause();
+    trackTwo.play();
+  })
+}
 
 function init() {
   insertBricks();
+  addListenersToGuitars();
   //grab the needles
   let secondNeedle = document.querySelector(".second-needle");
   let minuteNeedle = document.querySelector(".minute-needle");
